82487d27fe6bc5076eaa7fc8e5828cd59d97b1a9
yuubinnkyoku <yuubinnkyoku0730@gmail.com>
2025-06-27 16:17:07 +0900

    Discordボットの遅延情報取得機能を追加

--- api.py
+++ api.py
@@ -1,59 +1,32 @@
-import traceback
+import json
+import urllib.request
 
-import discord
-import requests
-from discord.ext import commands
+try
+    url = 'https://www.train-guide.westjr.co.jp/api/v3/kobesanyo.json'
+    url_st = url.replace('.json','_st.json')
+    res = urllib.request.urlopen(url)
+    res_st = urllib.request.urlopen(url_st)
+    data = json.loads(res.read().decode('utf-8'))
+    data_st = json.loads(res_st.read().decode('utf-8'))
 
-from env.config import Config
+    dictst = {}
 
-config = Config()
+    for station in data_st['stations']:
+        dictst[station['info']['code']] = station['info']['name']
 
-token = config.token
+    for item in data['trains']:
+        if item['delayMinutes'] > 0:
+            stn = item['pos'].split('_')
+            try:
+                position = dictst[stn[0]] + '辺り'
+            except KeyError:
+                position = "どこかよくわかんない"
+            tc=item['typeChange']
+            if tc == " ":
+                tc=''
+            print(f"{item['displayType']} {item['dest']['text']}行き {tc} {item['no']} {item['delayMinutes']}分遅れ {position}")
 
-
-intents = discord.Intents.default()
-bot = commands.Bot(command_prefix="/", intents=intents)
-
-@bot.tree.error
-async def on_error(interaction, error):
-    err = "".join(traceback.format_exception(error))
-    embed = discord.Embed(description=f"```py\n{err}\n```"[:4095])
-    if interaction.response.is_done():
-        await interaction.channel.send("An error has occurred.", embed=embed)
-    else:
-        await interaction.response.send_message("An error has occurred.", embed=embed)
-
-
-@bot.event
-async def on_ready():
-    print("ログインしました")
-    # スラッシュコマンドを同期
-    await bot.tree.sync()
-
-
-@bot.tree.command(name="jr_west_delay", description="JR西日本の遅延情報を取得します。")
-async def test(interaction: discord.Interaction):
-    url = "https://www.train-guide.westjr.co.jp/api/v3/kobesanyo.json"
-    # GETリクエストを送信
-    response = requests.get(url)
-    # レスポンスをJSON形式で取得
-    data = response.json()
-
-    trains = data["trains"]
-
-    delay_messages = []
-    for train in trains:
-        delay = train["delayMinutes"]
-        if delay != 0:
-            delay_messages.append(
-                f"{train['displayType']} {train['dest']['text']}行き {train['typeChange']} {train['no']}: {delay}分遅れ"
-            )
-
-    if delay_messages:
-        content = "\n".join(delay_messages)
-    else:
-        content = "現在、遅延情報はありません。"
-    await interaction.response.send_message(content)
-
-
-bot.run(token=token)
+except urllib.error.HTTPError as err:
+    print('HTTPError: ', err)
+except json.JSONDecodeError as err:
+    print('JSONDecodeError: ', err)
--- main.py
+++ main.py
@@ -1,6 +1,59 @@
-def main():
-    print("Hello from jr!")
+import traceback
+
+import discord
+import requests
+from discord.ext import commands
+
+from env.config import Config
+
+config = Config()
+
+token = config.token
 
 
-if __name__ == "__main__":
-    main()
+intents = discord.Intents.default()
+bot = commands.Bot(command_prefix="/", intents=intents)
+
+@bot.tree.error
+async def on_error(interaction, error):
+    err = "".join(traceback.format_exception(error))
+    embed = discord.Embed(description=f"```py\n{err}\n```"[:4095])
+    if interaction.response.is_done():
+        await interaction.channel.send("An error has occurred.", embed=embed)
+    else:
+        await interaction.response.send_message("An error has occurred.", embed=embed)
+
+
+@bot.event
+async def on_ready():
+    print("ログインしました")
+    # スラッシュコマンドを同期
+    await bot.tree.sync()
+
+
+@bot.tree.command(name="jr_west_delay", description="JR西日本の遅延情報を取得します。")
+async def test(interaction: discord.Interaction):
+    url = "https://www.train-guide.westjr.co.jp/api/v3/kobesanyo.json"
+    # GETリクエストを送信
+    response = requests.get(url)
+    # レスポンスをJSON形式で取得
+    data = response.json()
+
+    trains = data["trains"]
+
+    delay_messages = []
+    for train in trains:
+        delay = train["delayMinutes"]
+        if delay != 0:
+            delay_messages.append(
+                f"{train['displayType']} {train['dest']['text']}行き {train['typeChange']} {train['no']}: {delay}分遅れ"
+            )
+
+    if delay_messages:
+        content = "\n".join(delay_messages)
+    else:
+        content = "現在、遅延情報はありません。"
+    await interaction.response.send_message(content)
+
+
+bot.run(token=token)
